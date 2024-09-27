513ca5b1adfe48d1a35913072030d92daee27828
fernandoggaitan <fernandoggaitan@gmail.com>
2024-09-27 18:13:22 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+//Java Script and XML
+export default function App(){
+
+  return (
+    <>
+      <h1 className="text-center"> Hola soy un componente llamado App </h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste doloremque deserunt, enim, velit culpa dolore voluptatem nostrum aliquam eveniet quam esse perspiciatis nihil harum minus id cumque corrupti numquam vitae! </p>
+      <br />
+    </>
+  )
+
+}
